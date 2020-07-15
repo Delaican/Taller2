9ad98bf2b2a4491a6ec2215e9046b593ce6c3c42
Kevin <kdlaikanj@gmail.com>
2020-07-15 10:47:09 -0500

    validación inputs 1.0

--- Taller5/scripts.js
+++ Taller5/scripts.js
@@ -28,3 +28,27 @@ negativo.addEventListener('click', () => {
         selects[j].setAttribute("disabled", "");
     }
 });
+
+function validar25caracteres(valinput){
+    if (valinput == ""){
+        alert("Debe rellenar este campo");
+        valinput.focus();
+        valinput.select();
+        return false;
+    } else if (valinput.length > 25) {
+        alert("El campo debe tener como máximo 25 caracteres");
+        valinput.focus();
+        valinput.select();
+        return false;
+    } else {return true;}
+};
+
+function validarCampos() {
+    var retorno;
+    var nombre = document.getElementById("nombre");
+    retorno = validar25caracteres(nombre.value);
+    var apellido = document.getElementById("apellido");
+    retorno = validar25caracteres(apellido.value);
+};
+
+//validarCampos();
