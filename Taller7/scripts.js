79932cafbddce4f5a67073cb09c59ce2544a66f4
Christian <chrismcruiz@gmail.com>
2020-08-05 15:42:12 -0500

    taller 7 terminado

--- Taller7/scripts.js
+++ Taller7/scripts.js
@@ -3,35 +3,43 @@ const disableds = Array.from(document.querySelectorAll('.disabled'));
 const selectDptos = document.getElementById('departamentos');
 const selectMun = document.getElementById('municipios');
 
-
-
 paises.addEventListener('change', (e) => {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://www.datos.gov.co/resource/xdk5-pm3f.json');
 
-   
+
     xhr.addEventListener('load', (data) => {
         const dataJSON = JSON.parse(data.target.response);
 
+        let dptos = [];
         for (i in dataJSON) {
-            let dptos = dataJSON.filter((e) => {
-               return e.departamento == dataJSON[i]['departamento']; 
-            });
-            console.log(dptos);
+            dptos.push(dataJSON[i]['departamento']);
         }
-        
-        const option = document.createElement('OPTION');
-        option.textContent = 'Perrito';
-        selectDptos.appendChild(option);
+
+        let unique = [...new Set(dptos)].sort();
+
+        for (i of unique) {
+            const option = document.createElement('OPTION');
+            option.textContent = i;
+            option.value = `${i.toLowerCase()}`; 
+            selectDptos.appendChild(option);
+        }
 
         for (let i = 0; i < disableds.length / 2; i++) {
             disableds[i].classList.remove('disabled');
         }
 
         selectDptos.addEventListener('change', (e) => {
-            const option = document.createElement('OPTION');
-            option.textContent = 'Menor';
-            selectMun.appendChild(option);
+            const dptoselected = selectDptos.value;
+
+            for (i in dataJSON) {
+                if (dataJSON[i].departamento.toLowerCase() == dptoselected) {
+                    const option = document.createElement('OPTION');
+                    option.textContent = dataJSON[i].municipio;
+                    selectMun.appendChild(option);
+                }
+            }
+
             for (let i = 2; i < disableds.length; i++) {
                 disableds[i].classList.remove('disabled');
             }
