6697802f92a9e173453568b38823fbbb1b37f4f4
Christian <chrismcruiz@gmail.com>
2020-08-04 22:55:19 -0500

    taller 7 casi terminado

--- Taller7/scripts.js
+++ Taller7/scripts.js
@@ -1,4 +1,42 @@
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-});
+// const paises = document.getElementById('paises');
+const disableds = Array.from(document.querySelectorAll('.disabled'));
+const MunContainer = document.getElementById('municipiosContainer');
+const selectDptos = document.getElementById('departamentos');
+const selectMun = document.getElementById('municipios');
+
+
+
+paises.addEventListener('change', (e) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.datos.gov.co/resource/xdk5-pm3f.json');
+
+    xhr.addEventListener('load', (data) => {
+        const dataJSON = JSON.parse(data.target.response);
+
+        const option = document.createElement('OPTION');
+        option.textContent = 'Peguelo';
+        selectDptos.appendChild(option);
+
+        for (let i = 0; i < disableds.length / 2; i++) {
+            disableds[i].classList.remove('disabled');
+        }
+
+        selectDptos.addEventListener('change', (e) => {
+            const option = document.createElement('OPTION');
+            option.textContent = 'Menor';
+            selectMun.appendChild(option);
+            for (let i = 2; i < disableds.length; i++) {
+                disableds[i].classList.remove('disabled');
+            }
+            $(document).ready(function () {
+                $('select').formSelect();
+            });
+        })
+
+        $(document).ready(function () {
+            $('select').formSelect();
+        });
+    });
+    xhr.send();
+})
+
